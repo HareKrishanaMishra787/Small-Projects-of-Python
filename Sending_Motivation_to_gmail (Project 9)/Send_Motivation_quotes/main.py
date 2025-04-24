acbad340684f42d73a9cd67d4311d60895b9770a
Hare Krishana Mishra <133776847+HareKrishanaMishra787@users.noreply.github.com>
2025-04-24 22:57:31 +0530

    Add files via upload

---
+++ Sending_Motivation_to_gmail (Project 9)/Send_Motivation_quotes/main.py
@@ -0,0 +1,21 @@
+import datetime as dt
+import random
+import smtplib
+my_email = "tarun96968@gmail.com"
+password= "srcv nbae lpoh fhxd"
+x = dt.datetime.now()
+
+
+if x.weekday() == 5:# Suppose today was saturday so it weekday num is 5
+    with open("quotes.txt") as quotes:
+        contents = quotes.readlines()
+        quote = random.choice(contents)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email,password=password)
+        connection.sendmail(from_addr=my_email,to_addrs="tarunmishra12122002@gmail.com"
+                            ,msg=f"Subject:best_wishes\n\n{quote}")
+
+
+
+
